029a521c293dd3deeaf85566fdc4d28b950bf0e1
chen  peleg surface <chenpeleg@yahoo.com>
2021-08-17 13:29:43 +0300

    card sizes

--- src/components/bird-card.jsx
+++ src/components/bird-card.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Box from '@material-ui/core/Box';
+
+
+import { Card } from '@material-ui/core';
+
 const BirdCard = (props) => {
-    return (<div> {props.bird.HebName}</div>)
+    return (<div>
+        <Box height="30vh">
+            {/* <Card color="primary"> */}
+            <b>{props.bird.HebrewName} </b>
+            {/* </Card> */}
+
+        </Box>
+    </div>)
 }
 export default BirdCard
--- src/layouts/birrd-card-container-layout.jsx
+++ src/layouts/birrd-card-container-layout.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import styles from './birdCardContainerLayout.module.scss';
 import getAllBirds from '../hoc/getbirdsdata';
 import BirdCard from '../components/bird-card';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 const BirdCardContainerLayout = () => {
     //const bird1 = 'Bird 1';
+    const classes = useStyles();
     const [...allBirds] = getAllBirds();
     console.log(allBirds)
 
@@ -12,11 +27,32 @@ const BirdCardContainerLayout = () => {
     //     height: '100px'
     // }
     return (<div className={styles.birdcardlaout} >
-        {allBirds.map((bird) => {
-            // eslint-disable-next-line no-lone-blocks
-            { return <BirdCard bird={bird}></BirdCard> }
-        })}
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                {allBirds.map((bird) => {
+                    // eslint-disable-next-line no-lone-blocks
+                    {
+                        return (
+                            <Grid item xs={3}>
+                                <Paper className={classes.paper}>
 
+
+                                    <BirdCard bird={bird} key={bird.id}></BirdCard>
+
+                                </Paper>
+                            </Grid>
+
+
+                        )
+                    }
+                })}
+            </Grid>
+        </div>
+        <Box flexDirection="row" >
+
+
+
+        </Box>
     </div>)
 
 }
